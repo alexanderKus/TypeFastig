c958e59070034633773abf4375064f0122533ee6
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2023-08-29 14:33:34 +0200

    Add more samples

--- fastTyping-Client/src/app/shared/model-factory/model-factory.ts
+++ fastTyping-Client/src/app/shared/model-factory/model-factory.ts
@@ -2,17 +2,18 @@ import { Model } from '../../features/models/model';
 
 export class ModelFactory {
   private static prevIndex: number = 0;
-  private static texts = new Map<number, string>([
-    // TODO: Create more templates
+  private static textsEasy = new Map<number, string>([
     [0, 'siema'],
     [1, 'aleksander'],
     [2, 'olek'],
     [3, 'kuznia'],
-    /*
+  ]);
+  private static texts = new Map<number, string>([
     [
-      1,
-      `static void *proc_keys_start(struct seq_file *p, loff_t *_pos)
-    __acquires(key_serial_lock) 
+      0,
+      `
+static void *proc_keys_start(struct seq_file *p, loff_t *_pos)
+  __acquires(key_serial_lock) 
 {
   key_serial_t pos = *_pos;
 	struct key *key;
@@ -25,12 +26,30 @@ export class ModelFactory {
   *_pos = key->serial;
   return &key->serial_node;
 }`,
-    ], */
+    ],
+    [
+      1,
+      `
+class AdagradOptimizer(optimizer.Optimizer):
+  def __init__(self, learning_rate, initial_accumulator_value=0.1,
+    use_locking=False, name="Adagrad"):
+    if initial_accumulator_value <= 0.0:
+      raise ValueError("initial_accumulator_value must be positive: %s" %
+                        initial_accumulator_value)
+  super(AdagradOptimizer, self).__init__(use_locking, name)
+  self._learning_rate = learning_rate
+  self._initial_accumulator_value = initial_accumulator_value
+  self._learning_rate_tensor = None
+`,
+    ],
+    [2, `beka`],
   ]);
 
-  public static createModel(): Model {
-    const randomIndex = this.getRandomIndex();
-    const text = this.texts.get(randomIndex);
+  public static createModel(easyMode: boolean = false): Model {
+    const randomIndex = this.getRandomIndex(easyMode);
+    let text: string = easyMode
+      ? this.textsEasy.get(randomIndex)!
+      : this.texts.get(randomIndex)!;
     return new Model(text);
   }
 
@@ -38,12 +57,13 @@ export class ModelFactory {
     return new Model('');
   }
 
-  private static getRandomIndex(): number {
-    const randomIndex = Math.floor(Math.random() * this.texts.size);
-    if (this.prevIndex !== randomIndex) {
-      this.prevIndex = randomIndex;
-      return randomIndex;
-    }
-    return this.getRandomIndex();
+  private static getRandomIndex(easyMode: boolean = false): number {
+    const dirSize: number = easyMode ? this.textsEasy.size : this.texts.size;
+    let randomIndex: number;
+    do {
+      randomIndex = Math.floor(Math.random() * dirSize);
+    } while (this.prevIndex === randomIndex);
+    this.prevIndex = randomIndex;
+    return randomIndex;
   }
 }
