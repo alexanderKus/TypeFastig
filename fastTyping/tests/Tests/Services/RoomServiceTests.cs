847dedd5473a098b95485fa7ceda257ffdadc643
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2024-01-07 14:16:30 +0100

    FIx tests

--- fastTyping/tests/Tests/Services/RoomServiceTests.cs
+++ fastTyping/tests/Tests/Services/RoomServiceTests.cs
@@ -21,14 +21,10 @@ namespace Tests.Services
             var player1 = PlayerHelper.CreatePlayer(1);
             var player2 = PlayerHelper.CreatePlayer(2);
             var player3 = PlayerHelper.CreatePlayer(3);
-            var player4 = PlayerHelper.CreatePlayer(4);
-            var player5 = PlayerHelper.CreatePlayer(5);
 
             var room = _roomService.JoinRoom(player1);
             _roomService.JoinRoom(player2);
             _roomService.JoinRoom(player3);
-            _roomService.JoinRoom(player4);
-            _roomService.JoinRoom(player5);
 
             Assert.True(room.IsFull);
         }
@@ -52,15 +48,11 @@ namespace Tests.Services
             var player2 = PlayerHelper.CreatePlayer(2);
             var player3 = PlayerHelper.CreatePlayer(3);
             var player4 = PlayerHelper.CreatePlayer(4);
-            var player5 = PlayerHelper.CreatePlayer(5);
-            var player6 = PlayerHelper.CreatePlayer(6);
 
             var room1 = _roomService.JoinRoom(player1);
             _roomService.JoinRoom(player2);
             _roomService.JoinRoom(player3);
-            _roomService.JoinRoom(player4);
-            _roomService.JoinRoom(player5);
-            var room2 = _roomService.JoinRoom(player6);
+            var room2 = _roomService.JoinRoom(player4);
 
             Assert.NotEqual(room1, room2);
         }
